5edcc6f50ba151d12454d7746b8b7b70a0642bed
Neerav <fbnirav@gmail.com>
2018-10-16 10:26:44 +0530

    view directory added

--- app/src/main/java/com/codefuelindia/dnote/constants/view/MainActivity.kt
+++ app/src/main/java/com/codefuelindia/dnote/constants/view/MainActivity.kt
@@ -1,8 +1,8 @@
-package com.codefuelindia.dnote
+package com.codefuelindia.dnote.constants.view
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import com.codefuelindia.dnote.constants.MyConstants
+import com.codefuelindia.dnote.R
 
 class MainActivity : AppCompatActivity() {
 
